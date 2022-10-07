9638cf46a4b9c8f08afd1b510da3a3d1464223be
Gideon <gidewadejob@gmail.com>
2022-10-07 21:45:52 +0100

    MultiAlley2.java DONE deadlocks 10 out of 10 attempts

--- MultiAlley2.java
+++ MultiAlley2.java
@@ -16,13 +16,25 @@ public class MultiAlley2 extends Alley {
 
     /* Block until car no. may enter alley */
     public void enter(int no) throws InterruptedException {
+        System.out.println();
+        System.out.println("-------------------");
+        System.out.println("Semaphore up: " + upSem.s);
+        System.out.println("Semaphore down: " + downSem.s);
         if (no < 5) {
+            System.out.println("----- IN IF -----");
+            System.out.println("car: " + no);
+            System.out.println("up: " + up);
+            System.out.println("down: " + down);
             downSem.P();
-            Thread.sleep(1000);
             if (down == 0) upSem.P();    // block for up-going cars
+            Thread.sleep(900);
             down++;
             downSem.V();
         } else {
+            System.out.println("----- IN ELSE -----");
+            System.out.println("car: " + no);
+            System.out.println("up: " + up);
+            System.out.println("down: " + down);
             upSem.P();
             if (up == 0) downSem.P();    // block for down-going cars
             up++;
